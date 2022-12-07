573584ad4f44c556ab2c69008621b87fd30968fe
mariaemf <mariaeduardamartinsf@gmail.com>
2022-12-07 19:45:41 -0300

    finalização do mesmo. Desenvolvimento para buscarmais conhecimentos com Chackra ui

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import {
   RadioGroup,
   Radio,
   Button,
+  Checkbox,
 } from "@chakra-ui/react";
 
 function App() {
@@ -17,10 +18,10 @@ function App() {
       <Center
         as="header" //div header centalizada
         h={150} //150 px
-        bg="teal.500" //background
-        color="white"
+        bg="pink.200" //background
+        color="pink.600"
         fontWeight="bold"
-        fontSize="7x1"
+        fontSize="35"
         pb="8"
       >
         Formulário
@@ -28,30 +29,98 @@ function App() {
       <Flex
         align="Center"
         justify="center"
-        bg="blackAlpha.200"
+        bg="purple.100"
         h="calc(100vh - 150px)"
       >
         <Center
           w="100%"
           maxW={840}
-          bg="white" //background color
+          bg="pink.50" //background color
           top={100} //topo
           position="absolute"
           borderRadius={5}
           p="6" // padding (quando tem aspas bast amultiplicar por 4 = 6x4 = 24)
-          boxShadow="0 1px 2px #ccc"
+          boxShadow="0 1px 2px #e23bc3"
         >
           <FormControl display="flex" flexDir="column" gap="4">
             <HStack spacing="4">
               <Box w="100%">
-                <FormLabel htmlFor="nome">Nome Completo </FormLabel>
+                <FormLabel htmlFor="nome" color="pink.600">
+                  Nome Completo{" "}
+                </FormLabel>
                 <Input id="nome" />
               </Box>
               <Box w="100%">
-                <FormLabel htmlFor="email">Seu E-mail </FormLabel>
+                <FormLabel htmlFor="email" color="pink.600">
+                  Seu E-mail{" "}
+                </FormLabel>
                 <Input id="email" type="email" />
               </Box>
             </HStack>
+            <HStack spacing="4">
+              <Box w="100%">
+                <FormLabel htmlFor="nasc" color="pink.600">
+                  Data de Nascimento
+                </FormLabel>
+                <Input id="nasc" type="date" />
+              </Box>
+              <Box w="100%">
+                <FormLabel htmlFor="naturalidade" color="pink.600">
+                  Naturalidade
+                </FormLabel>
+                <Input id="natural" />
+              </Box>
+            </HStack>
+            <HStack spacing="4">
+              <Box w="100%">
+                <FormLabel htmlFor="celular" color="pink.600">
+                  Celular
+                </FormLabel>
+                <Input id="cel" type="number" />
+              </Box>
+              <Box w="100%">
+                <FormLabel htmlFor="telefone" color="pink.600">
+                  Telefone
+                </FormLabel>
+                <Input id="telefone" type="number" />
+              </Box>
+            </HStack>
+            <HStack spacing="4">
+              <Box w="100%">
+                <FormLabel htmlFor="endereço" color="pink.600">
+                  Endereço
+                </FormLabel>
+                <Input id="endereço" />
+              </Box>
+              <Box w="100%">
+                <FormLabel htmlFor="cidade" color="pink.600">
+                  Cidade
+                </FormLabel>
+                <Input id="cidade" />
+              </Box>
+            </HStack>
+            <RadioGroup defaultValue="Masculino">
+              <HStack spacing="24px">
+                <Radio value="Masculino">Masculino</Radio>
+                <Radio value="Feminino">Feminino</Radio>
+                <Radio value="Outro">Outro</Radio>
+              </HStack>
+              <HStack justify="center">
+                <Button
+                  w={240}
+                  p="6"
+                  type="submit"
+                  bg="pink.500"
+                  color="white"
+                  fontWeight="bold"
+                  fontSize="xl"
+                  mt="17"
+                  _hover={{ bg: "pink.900" }}
+                >
+                  Enviar Formulário
+                </Button>
+              </HStack>
+            </RadioGroup>
           </FormControl>
         </Center>
       </Flex>
